0b7b7be9dabe9c0c67a35c0b62bd50b3a0e21b52
brandn03 <w0783032@myscc.ca>
2025-11-13 15:43:38 -0500

    - Updated game list html. - Created CRUD operations for game review service

--- src/app/services/game-review.ts
+++ src/app/services/game-review.ts
@@ -7,12 +7,36 @@ import {Game} from '../models/game';
   providedIn: 'root'
 })
 export class GameReview {
+
+  gameReviews : Game[] = data;
   constructor() {
   }
   getGameReviews(): Observable<Game[]>
   {
-    return of(data);
+    return of(this.gameReviews);
   }
+  addGameReview(newGameReview:Game) : Observable<Game[]>{
+    this.gameReviews.push(newGameReview);
+    return of(this.gameReviews);
+  }
+  updateGameReview(updatedGameReview : Game) : Observable<Game[]>
+  {
+    const index = this.gameReviews.findIndex(review => review.id === updatedGameReview.id);
+    if (index !== -1) {
+      this.gameReviews[index] = updatedGameReview;
+    }
+    return of(this.gameReviews);
+  }
+  deleteGameReview(gameID: number) : Observable<Game[]>
+  {
+    this.gameReviews = this.gameReviews.filter(review => review.id !== gameID);
+    return of(this.gameReviews);
+  }
+  getGameReviewById(gameID: number) : Observable<Game | undefined> {
+    const review = this.gameReviews.find(game => game.id === gameID);
+    return of(review);
+  }
+
 
 
 }
