9c195211f465fbbe35030c64a5c027ce061048a9
brandn03 <w0783032@myscc.ca>
2025-11-13 16:46:20 -0500

    - Created Router and routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideRouter, Routes } from
+    '@angular/router';
+import {GameList} from './app/game-list/game-list';
+import {GameListItem} from './app/game-list-item/game-list-item';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  { path: 'Games', component: GameList },
+  { path: 'Games/:id', component: GameListItem
+  }
+];
+
+bootstrapApplication(App, {
+  providers: [provideRouter(routes)]
+});
